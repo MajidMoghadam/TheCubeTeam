cdf1b48d5a6283e75ebab8ebb19be36fb94ec634
MajidMoghadam <majidmoghadam@hotmail.com>
2021-12-11 12:53:46 -0500

    added color changing script
    when user clicks on scene the surface of the cube changes to a random color

---
+++ TheCubeTeamProject/Assets/Scripts/ColorChanger.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorChanger : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButton("Fire1"))
+        {
+            GetComponent<Renderer>().material.color = GetRandomColor();
+        }
+    }
+
+    private Color GetRandomColor()
+    {
+        return new Color(UnityEngine.Random.Range(0,1f),
+                        UnityEngine.Random.Range(0, 1f),
+                        UnityEngine.Random.Range(0, 1f));
+    }
+}
